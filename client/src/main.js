3b3cfc1ec378e6ed592cf2551b76412dc11fb15d
EmanOthman21 <eothman21@gmail.com>
2020-06-18 21:01:45 +0200

    Install some dependencies
    bootstrap - axios - vue  styleguide -jest coverage

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false;
 
