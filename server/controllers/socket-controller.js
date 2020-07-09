1be50297ab324a3e58ade20f09a0997a0be9f011
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-07-09 04:15:23 +0200

    reply-comment socket

--- server/controllers/socket-controller.js
+++ server/controllers/socket-controller.js
@@ -1,38 +1,60 @@
 var socket = require("socket.io");
 const userModel = require("../models/db").user;
+const pinModel = require("../models/db").pin;
 module.exports = function (server) {
   var io = socket(server);
   io.on("connection", function (socket) {
     socket.on("setUserId", async function (data) {
-      let user = await userModel.findById(data.userId);
+      const token = data.token;
+      const decoded = await jwt.verify(token, process.env.jwtsecret);
+      userId = decoded._id;
+      let user = await userModel.findById(userId);
       if (user) {
         user.socketId = socket.id;
         await user.save();
       }
     });
     socket.on("comment", async function (data) {
-      let user = await userModel.findById(data.userId);
+      let commentText = data.commentText;
+      let pinId = data.pinId;
+      let pin = await pinModel.findById(pinId);
       const token = data.token;
-      const decoded = jwt.verify(token, process.env.jwtsecret);
+      const decoded = await jwt.verify(token, process.env.jwtsecret);
       commentterId = decoded._id;
       let commentter = await userModel.findById(commentterId);
-      if (user && commentter) {
-        let socketId = user.socketId;
-        if (socketId && socketId != "none") {
-          console.log(socketId);
-          socket.broadcast.to(socketId).emit("sendComment", {
-            commentText: data.text,
-            commentterName: commentter.firstName + " " + commentter.lastName,
-            commentterImage: commentter.profileImage,
-            pinId: data.pinId,
-            date: Date.now(),
-          });
-        }
+      if (commentter && pin && commentText) {
+        io.sockets.emit("sendComment", {
+          commentText: data.text,
+          commentterName: commentter.firstName + " " + commentter.lastName,
+          commentterImage: commentter.profileImage,
+          pinId: data.pinId,
+          date: Date.now(),
+        });
+      }
+    });
+    socket.on("reply", async function (data) {
+      let replyText = data.replyText;
+      let pinId = data.pinId;
+      let commentId = data.commentId;
+      let pin = await pinModel.findById(pinId);
+      const token = data.token;
+      const decoded = await jwt.verify(token, process.env.jwtsecret);
+      replierId = decoded._id;
+      let replier = await userModel.findById(replierId);
+      if (replier && pin && replyText) {
+        io.sockets.emit("sendReply", {
+          replyText: replyText,
+          replierName: replier.firstName + " " + replier.lastName,
+          replierImage: replier.profileImage,
+          pinId: data.pinId,
+          commentId: commentId,
+          date: Date.now(),
+        });
       }
     });
     socket.on("clearUserId", async function (data) {
       const token = data.token;
-      const decoded = jwt.verify(token, process.env.jwtsecret);
+      const decoded = await jwt.verify(token, process.env.jwtsecret);
       userId = decoded._id;
       let user = await userModel.findById(userId);
       if (user) {
