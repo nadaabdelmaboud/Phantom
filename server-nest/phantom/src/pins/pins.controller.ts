a0af114802db4d8fbd02bf9cbd3daaee7d88fdc2
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-07-13 11:54:23 +0200

    Http exception filter

--- server-nest/phantom/src/pins/pins.controller.ts
+++ server-nest/phantom/src/pins/pins.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseFilters,
+  ForbiddenException,
+} from '@nestjs/common';
 import { createPinDto } from './dto/create-pin.dto';
+import { HttpExceptionFilter } from '../shared/http-exception.filter';
+
+@UseFilters(new HttpExceptionFilter())
 @Controller('pins')
 export class PinsController {
   @Post()
   createPin(@Body() createPinDto: createPinDto): String {
-    return createPinDto.note;
+    if (createPinDto.note == 'nada') {
+      return 'done';
+    } else {
+      throw new ForbiddenException({ message: 'you are not nada' });
+    }
   }
 }
--- server-nest/phantom/src/pins/pins.module.ts
+++ server-nest/phantom/src/pins/pins.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PinsController } from './pins.controller';
 import { PinsService } from './pins.service';
-
+import { HttpExceptionFilter } from '../shared/http-exception.filter';
 @Module({
-  imports: [],
+  imports: [HttpExceptionFilter],
   controllers: [PinsController],
   providers: [PinsService],
 })
