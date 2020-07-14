aa96c5d9ed618a6c361160df82d78fdfdf53cba0
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-07-14 16:16:56 +0200

    boards routes

--- server-nest/phantom/src/board/board.controller.ts
+++ server-nest/phantom/src/board/board.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseFilters,
+  ForbiddenException,
+  NotAcceptableException,
+  Param,
+  Get,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from '../shared/http-exception.filter';
+import { BoardService } from './board.service';
 
+@UseFilters(new HttpExceptionFilter())
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(private BoardService: BoardService) {}
+  @Post('/me/boards')
+  async createBoard(@Body('name') name: string) {
+    let userId = '5ef10225f775502d20121345';
+    let createdBoard = await this.BoardService.createBoard(name, userId);
+    if (createdBoard) {
+      return createdBoard;
+    } else {
+      throw new NotAcceptableException({ message: 'board not created' });
+    }
+  }
+  @Get('/me/boards')
+  async getCurrentUserBoards() {
+    let userId = '5ef10225f775502d20121345';
+    let boards = await this.BoardService.getCurrentUserBoards(userId);
+    if (boards && boards.length != 0) {
+      return boards;
+    } else {
+      throw new NotFoundException({ message: 'no boards' });
+    }
+  }
+}
