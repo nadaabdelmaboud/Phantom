06de8cd7e921282b1c367311aba77151e5486077
Nihal Mansour <nihalmansour0599@gmail.com>
2020-08-01 15:30:16 +0200

    display home cards with fixed width

--- client/src/views/UserHome.vue
+++ client/src/views/UserHome.vue
@@ -1,11 +1,63 @@
 <template>
   <div class="home">
     <h1>Hello Phantom 😍😍</h1>
-    <HomeCard v-for="homecard in cards"
-       :key="homecard._id"
-       :cardImage="homecard.imageId" />
+    <div class="container">
+        <HomeCard
+        class="box"
+          v-for="homecard in cards"
+          :key="homecard._id"
+          :cardImage="homecard.imageId"
+        />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../scss/_Colors";
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  min-height: 100vh;
+
+}
+.container{
+position: relative;
+max-width: 100%;
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(250px , 1fr));
+grid-template-rows:  minmax(100px , auto);
+margin: 15px;
+grid-auto-flow: dense;
+grid-gap: 10px;
+}
+.container .box {
+  padding: 20px;
+  display: grid;
+  place-items: center;
+  text-align: center;
+  transition: 0.5s;
+
+}
+.container .box:hover {
+  background: $lightgrey;
+}
+@media (max-width: 990px) {
+  .container {
+  grid-template-columns: repeat(auto-fill, minmax(50% , 1fr));
+grid-template-rows:  minmax(auto , auto);
+  }
+  .container .box {
+    grid-column:unset !important ;
+    grid-row: unset !important;
+  }
+}
+</style>
 
 <script>
 import HomeCard from "../components/HomeCard";
@@ -15,10 +67,10 @@ export default {
   components: {
     HomeCard
   },
-    mounted() {
+  mounted() {
     this.$store.dispatch("homeCards/userHome");
   },
-    computed: {
+  computed: {
     ...mapGetters({
       cards: "homeCards/userHomePage"
     })
