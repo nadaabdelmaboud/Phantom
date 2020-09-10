31a42d38758076e45dd8c9c174a417e01500e7df
ayaabohadima <ayasabohadima@gmail.com>
2020-09-10 03:34:01 +0200

    edits in notification

--- server-nest/phantom/src/models/user.schema.ts
+++ server-nest/phantom/src/models/user.schema.ts
@@ -11,6 +11,7 @@ export const User = new Schema({
   followingTopics: Array(mongoose.Types.ObjectId),
   location: String,
   activateaccount: Boolean,
+  notificationCounter: Number,
   activity: Boolean,
   invitation: Boolean,
   boardUpdate: Boolean,
