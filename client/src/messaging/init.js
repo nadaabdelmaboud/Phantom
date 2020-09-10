123e78ef89064ec045fff166da63a0471b1de905
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-09-10 12:09:26 +0200

    edits

--- client/src/messaging/init.js
+++ client/src/messaging/init.js
@@ -9,7 +9,7 @@ export function get_messaging() {
 // Your web app's Firebase configuration
 export function initializeFirebase() {
   if (firebase.messaging.isSupported()) {
-    var firebaseConfig ={
+    var firebaseConfig = {
       apiKey: "AIzaSyDJGcM4-rKBLozMQu1uJmTORQPnQKVkS_M",
       authDomain: "phantom-30457.firebaseapp.com",
       databaseURL: "https://phantom-30457.firebaseio.com",
@@ -17,28 +17,25 @@ export function initializeFirebase() {
       storageBucket: "phantom-30457.appspot.com",
       messagingSenderId: "822902693694",
       appId: "1:822902693694:web:c433556b3c92dcc483966f",
-      measurementId: "G-BJ0XRKXP5E"
-    };;
+      measurementId: "G-BJ0XRKXP5E",
+    };
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
     messaging = firebase.messaging();
     messaging.onMessage((payload) => {
       console.log("Message received. ", payload);
-      // var sender = JSON.parse(payload.data);
-    //   var notificationTitle = payload.data.title;
-    //   var notificationOptions = {
-    //     body: payload.data.body,
-    //   };
-    //   var notification = new Notification(
-    //     notificationTitle,
-    //     notificationOptions
-    //   );
-    //   notification.onclick = function (event) {
-    //     event.preventDefault(); // prevent the browser from focusing the Notification's tab
-    //     window.open(payload.data.click_action, "_blank");
-    //     notification.close();
-    //   };
+
+      var notificationTitle = payload.data.title;
+      var notificationOptions = {
+        body: payload.data.body,
+      };
+      new Notification(notificationTitle, notificationOptions);
+      // notification.onclick = function(event) {
+      //   event.preventDefault(); // prevent the browser from focusing the Notification's tab
+      //   window.open(payload.data.click_action, "_blank");
+      //   notification.close();
+      // };
     });
     messaging
       .requestPermission()
@@ -52,9 +49,11 @@ export function initializeFirebase() {
       })
       .then((token) => {
         console.log(token);
-        axios.defaults.headers.common["Authorization"] = localStorage.getItem("userToken");;
+        axios.defaults.headers.common["Authorization"] = localStorage.getItem(
+          "userToken"
+        );
         axios
-          .put("me/"+token)
+          .put("me/" + token)
           .then((response) => {
             console.log("response", response);
           })
