38957818da83c432c539c5a17209f0b5d7395e1c
MENNA123MAHMOUD <42938021+MENNA123MAHMOUD@users.noreply.github.com>
2020-09-14 10:34:01 +0200

    images fix

--- client/src/mixins/getImage.js
+++ client/src/mixins/getImage.js
@@ -2,10 +2,10 @@ export default {
   methods: {
     getImage(imageId, topic = "") {
       if (topic != "") {
-        return "http://localhost:3000/api/image/ ?topic=" + topic;
+        return "http://localhost:3000/api/image/%20?topic=" + topic;
       }
       if (imageId == "") {
-        return "http://localhost:3000/api/image/ ";
+        return "http://localhost:3000/api/image/none";
       }
       return "http://localhost:3000/api/image/" + imageId;
     },
