d3f3d3bbc31b9ed303aae58be78a3ff5b2be37a7
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-09-22 01:47:03 +0200

    console edit

--- client/src/mixins/getImage.js
+++ client/src/mixins/getImage.js
@@ -7,7 +7,6 @@ export default {
       if (imageId == "") {
         return "http://localhost:3000/api/image/%20";
       }
-      console.log("weeeeeeeeeeeeeeeeeeeeeeeee");
       return "http://localhost:3000/api/image/" + imageId;
     },
   },
