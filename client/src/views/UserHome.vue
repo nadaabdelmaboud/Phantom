d4df9b3eb75005b0202a4fb7b5891b13b3289548
MENNA123MAHMOUD <42938021+MENNA123MAHMOUD@users.noreply.github.com>
2020-09-26 23:59:10 +0200

    home changes

--- client/src/views/UserHome.vue
+++ client/src/views/UserHome.vue
@@ -75,33 +75,34 @@ import { default as isLoggedIn } from "../mixins/isLoggedIn";
 export default {
   name: "UserHome",
   data: function() {
-    return {
-    };
+    return {};
   },
   components: {
     HomeCard,
-    Loading
+    Loading,
   },
   mixins: [isLoggedIn],
   computed: {
     ...mapGetters({
       cards: "homeCards/userHomePage",
-      homeLoading: "homeCards/homeLoading"
+      homeLoading: "homeCards/homeLoading",
     }),
     ...mapState({
-      userKey: state => state.user.userKey
-    })
+      userKey: (state) => state.user.userKey,
+    }),
   },
   mounted() {
-    this.$store.dispatch("homeCards/userHome");
     setTimeout(() => {
-      this.$store.dispatch("homeCards/userGenerateCards", 10);
-    }, 3000);
+      this.$store.dispatch("homeCards/userHome");
+      setTimeout(() => {
+        this.$store.dispatch("homeCards/userGenerateCards", 10);
+      }, 3000);
+    }, 1000);
   },
   methods: {
     showTopics() {
       this.$store.commit("popUpsState/toggleTopicsPopup");
-    }
-  }
+    },
+  },
 };
 </script>
